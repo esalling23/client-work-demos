e7186d16a7c190e388a4e98cc169fff6db62641e
Eron Salling <esalling23@gmail.com>
2025-01-12 18:18:00 -0500

    Updates redirect on TooSmallScreen component

--- app/common/TooSmallScreen.jsx
+++ app/common/TooSmallScreen.jsx
@@ -4,11 +4,11 @@ import { redirect } from "next/navigation"
 const TooSmallScreen = () => {
 	return (
 		<div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-			<h3>Sorry, these games are only supported on windows wider than {SCREEN_WIDTH} pixels.</h3>
+			<h3>Woops! These games are only supported on screens wider than {SCREEN_WIDTH} pixels.</h3>
 			<button 
 				className="rounded bg-yellow-500 hover:bg-yellow-400 hover:scale-1.1 py-2 px-6"
-				onClick={() => redirect('?mobile=true')}
-			>Take me to the mobile demos</button>
+				onClick={() => redirect('https://eronsalling.me/#/projects')}
+			>Check out something else</button>
 		</div>
 	)
 }
